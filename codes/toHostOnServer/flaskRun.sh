3ae681094ce08544580dab9c9c63970c8b5a6741
Rimesh <rimeshlwagun10@gmail.com>
2021-08-11 23:20:29 +0545

    uupate ...

--- codes/toHostOnServer/flaskRun.sh
+++ codes/toHostOnServer/flaskRun.sh
@@ -1,4 +1,4 @@
 export FLASK_APP=silentApp		#name of main script to run
 export FLASK_ENV=development	#deploying as development environment
-#flask run						#runs the flask app
-flask run --host=0.0.0.0	#tells OS to listen on all public IPs, enabling us to access through phone.
+flask run						#runs the flask app
+# flask run --host=0.0.0.0	#tells OS to listen on all public IPs, enabling us to access through phone.
